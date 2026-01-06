97bb3435b75a8ff1370c5b08f1244755e6922cff
Alex-nature <cht312511@163.com>
2026-01-06 17:47:23 +0800

    update load file

--- src/utils/dataset.py
+++ src/utils/dataset.py
@@ -2,6 +2,7 @@ import numpy as np
 import torch
 import torch.utils.data as data
 import pandas as pd
+import os
 import utils.tools as tools
 
 class UCFDataset(data.Dataset):
@@ -17,12 +18,22 @@ class UCFDataset(data.Dataset):
         elif test_mode == False:
             self.df = self.df.loc[self.df['label'] != 'Normal']
             self.df = self.df.reset_index()
-        
+
     def __len__(self):
         return self.df.shape[0]
 
     def __getitem__(self, index):
-        clip_feature = np.load(self.df.loc[index]['path'])
+        # Convert path to match current environment
+        original_path = self.df.loc[index]['path']
+        if original_path.startswith('/home/xbgydx/Desktop/UCFClipFeatures/'):
+            # Replace with the correct local path relative to project root
+            relative_path = original_path.replace('/home/xbgydx/Desktop/UCFClipFeatures/', 'data/CLIP_Features/UCF-Crime/UCFClipFeatures/')
+            # Use os.path.join to handle different OS path separators
+            clip_path = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), relative_path)
+        else:
+            clip_path = original_path
+
+        clip_feature = np.load(clip_path)
         if self.test_mode == False:
             clip_feature, clip_length = tools.process_feat(clip_feature, self.clip_dim)
         else:
@@ -43,7 +54,22 @@ class XDDataset(data.Dataset):
         return self.df.shape[0]
 
     def __getitem__(self, index):
-        clip_feature = np.load(self.df.loc[index]['path'])
+        # Convert path to match current environment
+        original_path = self.df.loc[index]['path']
+        if original_path.startswith('/home/xbgydx/Desktop/XDTrainClipFeatures/'):
+            # Replace with the correct local path relative to project root
+            relative_path = original_path.replace('/home/xbgydx/Desktop/XDTrainClipFeatures/', 'data/CLIP_Features/XD-Violence/XDTrainClipFeatures/XDTrainClipFeatures/')
+            # Use os.path.join to handle different OS path separators
+            clip_path = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), relative_path)
+        elif original_path.startswith('/home/xbgydx/Desktop/XDTestClipFeatures/'):
+            # Handle test data path
+            relative_path = original_path.replace('/home/xbgydx/Desktop/XDTestClipFeatures/', 'data/CLIP_Features/XD-Violence/XDTestClipFeatures/')
+            # Use os.path.join to handle different OS path separators
+            clip_path = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), relative_path)
+        else:
+            clip_path = original_path
+
+        clip_feature = np.load(clip_path)
         if self.test_mode == False:
             clip_feature, clip_length = tools.process_feat(clip_feature, self.clip_dim)
         else:
